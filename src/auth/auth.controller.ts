95ed1dda25152c789bbbf42ed791a3e72c95656a
santyasm <yyasant5@gmail.com>
2025-10-02 13:15:18 -0300

    refactor(auth): simplify register method to use RegisterDto

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, Get, Query } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,15 +13,7 @@ export class AuthController {
   }
 
   @Post('register')
-  async register(
-    @Body()
-    body: {
-      name: string;
-      username: string;
-      email: string;
-      password: string;
-    },
-  ) {
+  async register(@Body() body: RegisterDto) {
     return this.authService.register(body);
   }
 
